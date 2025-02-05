e001816741f76697ac8cfcd2acb28d3c97f51c05
LeeSH <snghyun331@gmail.com>
2025-02-05 14:22:17 +0900

    feat: #3

---
+++ questions/00007-easy-readonly/solves/solve_one.ts
@@ -0,0 +1,18 @@
+import { Expect, Equal } from "../../../utils"
+
+type cases = [
+    Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+  ]
+  
+  interface Todo1 {
+    title: string
+    description: string
+    completed: boolean
+    meta: {
+      author: string
+    }
+  }
+  
+  
+  // ============= Your Code Here =============
+  type MyReadonly<T> = { readonly [k in keyof T]: T[k] }
